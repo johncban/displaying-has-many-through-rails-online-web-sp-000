6b5128e7227c1095f5c240ddc7fc264b251e9de0
Juan Carlo Banayo <johncban@gmail.com>
2020-08-31 12:26:05 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,3 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 end
-
-def last_updated
-  updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
-end
